49590ddcd7aec86def5b20e49a8c389731d86296
KishanSewnath <kishan.sewnath@student.hu.nl>
2016-10-04 15:16:24 +0200

    les 9 NS API opgehaald

---
+++ Les 9/les 9.py
@@ -0,0 +1,17 @@
+import requests
+import xmltodict
+
+auth_details = ('kishan.sewnath@student.hu.nl', 'zUHIqDhfCwuaiw8-VaLimK1sbPw2wb_BwEInwbLLmHtv6GjDId3idw')
+api_url = 'http://webservices.ns.nl/ns-api-avt?station=ut'
+response = requests.get(api_url, auth=auth_details)
+
+vertrekXML = xmltodict.parse(response.text)
+
+print('Dit zijn de vertrekkende treinen:')
+for vertrek in vertrekXML['ActueleVertrekTijden']['VertrekkendeTrein']:
+    eindbestemming = vertrek['EindBestemming']
+
+    vertrektijd = vertrek['VertrekTijd'] # 2016-09-27T18:36:00+0200
+    vertrektijd = vertrektijd[11:16] # 18:36
+
+    print('Om '+vertrektijd+' vertrekt een trein naar '+ eindbestemming)
